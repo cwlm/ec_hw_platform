2e26b57ad5e11a11668d210b19ae384246850821
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2023-12-04 00:17:58 +0800

    feat: CAN

---
+++ Inc/UserCAN.h
@@ -0,0 +1,16 @@
+//
+// Created by cwlm on 2023/12/4.
+//
+
+#ifndef EC_HW_PLATFORM_USERCAN_H
+#define EC_HW_PLATFORM_USERCAN_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void canFilterInit(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //EC_HW_PLATFORM_USERCAN_H
---
+++ Src/UserCAN.c
@@ -0,0 +1,34 @@
+//
+// Created by cwlm on 2023/12/4.
+//
+
+#include "main.h"
+#include "../Inc/UserCAN.h"
+
+extern CAN_HandleTypeDef hcan1;
+extern CAN_HandleTypeDef hcan2;
+
+void canFilterInit(void)
+{
+    CAN_FilterTypeDef filter;
+    filter.FilterIdHigh=0x0000;
+    filter.FilterIdLow=0x0000;
+    filter.FilterMaskIdHigh=0x0000;
+    filter.FilterMaskIdLow=0x0000;
+    filter.FilterFIFOAssignment=CAN_FILTER_FIFO0;
+    //filter.FilterBank;
+    filter.FilterMode=CAN_FILTERMODE_IDMASK;
+    filter.FilterScale=CAN_FILTERSCALE_32BIT;
+    filter.FilterActivation=CAN_FILTER_ENABLE;
+    filter.SlaveStartFilterBank=14;
+
+    filter.FilterBank=0;
+    HAL_CAN_ConfigFilter(&hcan1,&filter);
+    HAL_CAN_Start(&hcan1);
+    HAL_CAN_ActivateNotification(&hcan1,CAN_IT_RX_FIFO0_MSG_PENDING);
+
+    filter.FilterBank=14;
+    HAL_CAN_ConfigFilter(&hcan2,&filter);
+    HAL_CAN_Start(&hcan2);
+    HAL_CAN_ActivateNotification(&hcan2,CAN_IT_RX_FIFO0_MSG_PENDING);
+}
