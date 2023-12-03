d920df598338269f1e567c556332884ab5c0a2a9
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2023-12-04 00:02:00 +0800

    feat: data process

---
+++ Inc/DataProcess.h
@@ -0,0 +1,18 @@
+//
+// Created by cwlm on 2023/12/4.
+//
+
+#ifndef EC_HW_PLATFORM_DATAPROCESS_H
+#define EC_HW_PLATFORM_DATAPROCESS_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+float clamp(float val, float min, float max);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //EC_HW_PLATFORM_DATAPROCESS_H
