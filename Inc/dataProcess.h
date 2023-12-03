fe806021bdbe8ac98b153520f5511e01bb981532
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2023-12-03 16:59:34 +0800

    feat: clamp

---
+++ Inc/dataProcess.h
@@ -0,0 +1,10 @@
+//
+// Created by cwlm on 2023/12/3.
+//
+
+#ifndef EC_HW_PLATFORM_DATAPROCESS_H
+#define EC_HW_PLATFORM_DATAPROCESS_H
+
+float clamp(float val, float min, float max);
+
+#endif //EC_HW_PLATFORM_DATAPROCESS_H
---
+++ Src/dataProcess.c
@@ -0,0 +1,19 @@
+//
+// Created by cwlm on 2023/12/3.
+//
+
+#ifndef EC_HW_PLATFORM_DATAPROCESS_H
+#define EC_HW_PLATFORM_DATAPROCESS_H
+
+
+float clamp(float val, float min, float max) {
+    if (val < min) {
+        return min;
+    } else if (val > max) {
+        return max;
+    } else {
+        return val;
+    }
+}
+
+#endif //EC_HW_PLATFORM_DATAPROCESS_H
